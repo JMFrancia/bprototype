58954042d38f5b27e6a06f1f535e85dae859aee1
J.M.Francia <jmfrancia@jmfrancias-mbp.lan>
2019-02-11 10:10:20 -0500

    Initial commit

---
+++ Assets/Scripts/BaseCell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCell : MonoBehaviour
+{
+    virtual protected void OnTap() {}
+    virtual protected void OnHold() {}
+    virtual protected void OnDragOn(DraggableIcon icon) {}
+    virtual protected void OnDragOff() {}
+}
---
+++ Assets/Scripts/DraggableIcon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableIcon : MonoBehaviour
+{
+    [SerializeField] Sprite icon;
+
+
+}
